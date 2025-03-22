68f293262fe986ae14067fa4861b772e123db53e
Starscouts <starscouts@equestria.dev>
2025-03-22 18:25:45 +0100

    Prepare for shut down

--- client/assets/src/config.js
+++
@@ -1,5 +0,0 @@
-if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
-    window.SERVER = "http://127.0.0.1:8080";
-} else {
-    window.SERVER = "https://voice-api.floo.fi";
-}
